291893efdc3b8c7a9083fbfc283e9bd9acfe5f14
Oliver Metz <softwaredevdad@gmail.com>
2025-10-30 21:27:38 +0100

    generate prisma and setup first migration for initialization, update server.ts with all necessary packages for development mode and production ready security protection from various different attack methods

--- TaskManager-Backend/src/server.ts
+++ TaskManager-Backend/src/server.ts
@@ -1,13 +1,56 @@
 import express from "express";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { xss } from "express-xss-sanitizer";
+import hpp from "hpp";
+import rateLimit from "express-rate-limit";
 
 //#region Constants
 const app = express();
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
 //#endregion
 
 //#region Middleware
+// Global rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
+});
+
+// Security Middleware
+app.use(helmet()); // Set security HTTP headers
+app.use(xss()); // Data sanitization against XSS
+app.use(hpp()); // Prevent HTTP Parameter Pollution
+app.use("/api", limiter); // Apply rate limiting to all routes
+
+const env = process.env.NODE_ENV || "development"; // fallback
+if (env === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "device-remember-token",
+      "Access-Control-Allow-Origin",
+      "Origin",
+      "Accept",
+    ],
+  }),
+);
 //#endregion
 
 //#region Endpoints
---
+++ TaskManager-Backend/todo.md
@@ -0,0 +1,11 @@
+# 1. Install packages
+
+```bash
+npm install express dotenv cookie-parser cors helmet morgan hpp express-rate-limit
+```
+
+# 2. Install TypeScript types
+
+```bash
+npm install -D @types/express @types/cookie-parser @types/cors @types/helmet @types/morgan @types/hpp @types/express-rate-limit
+```
